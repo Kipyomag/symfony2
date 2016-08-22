67465eafa1c9369136ab8b34d32d1a30d456c98c
Kipyomag <yolemag@gmail.com>
2016-08-22 09:06:31 +0200

    ajout entity sheet

--- src/FrontBundle/Controller/SheetController.php
+++ src/FrontBundle/Controller/SheetController.php
@@ -1,10 +1,11 @@
 <?php
 namespace FrontBundle\Controller;
 
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use FrontBundle\Entity\Sheet;
 
 Class SheetController extends Controller
 {
@@ -12,6 +13,22 @@ Class SheetController extends Controller
     // lister les différentes fiches
     public function sheetListAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('FrontBundle:Sheet');
+
+        $sheets = $repository->getAll();
+
+
+
+        /*$sheet = new Sheet();1
+        $sheet->setName('MTV New York');
+        $sheet->setType('CD Audio');
+        $sheet->setArtist('Nirvana');
+        $sheet->setDuration('53:30');
+        $sheet->setReleased(new \DateTime(null));
+
+        $em->persist($sheet);
+        $em->flush();*/
 
         $session = $this->get('session');
         $session->set('machin', 'plop');
@@ -28,7 +45,15 @@ Class SheetController extends Controller
 
     public function sheetAction($id, Request $request)
     {
-        return $this->render('FrontBundle:Sheet:sheet.html.twig', array('id' => $id));
+        $repository = $this->getDoctrine()->getManager()->getRepository('FrontBundle:Sheet');
+        $sheet = $repository->find($id);
+
+        if (!$sheet){
+            throw new EntityNotFoundException();
+        }
+
+
+        return $this->render('FrontBundle:Sheet:sheet.html.twig', array('sheet' => $sheet));
     }
 }
 
